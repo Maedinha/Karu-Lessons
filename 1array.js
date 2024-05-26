b7e74510dc37dda43c42e3228794605b1ec6a536
Maedinha <gabrieltrevelim@gmail.com>
2024-05-26 13:39:53 +0900

    update 05/26

--- 1array.js
+++ 1array.js
@@ -1,12 +1,32 @@
+/////////////
+//Lesson 1//
+
 // const nota1 = 10;
 // const nota2 = 6.5;
 // const nota3 = 8;
 // const nota4 = 7.5;
 
-const notas = [10, 6.5, 8, 7.5]
+// const notas = [10, 6.5, 8, 7.5]
 
 //const media = (nota1 + nota2 + nota3 + nota4)/4
 
-const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+// const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
-console.log(media);gi
+// console.log(media);
+
+
+
+/////////////
+//Lesson 2//
+
+const grades = [10, 6.5, 8, 7.5]
+function average(grades) {
+    let sum = 0;
+    for (let i = 0; i < grades.length; i++) {
+        sum += grades[i];
+    }
+    return sum / grades.length;
+}
+
+var output = average(grades);
+console.log(output);
